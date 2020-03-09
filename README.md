c4fa5bc34ab35f8e0caf6265d7de39300176e028
Chuck Lorenz <chucklorenz1038@gmail.com>
2020-03-09 07:02:31 -0400

    Add index header and group header (#44)

--- README.md
+++ README.md
@@ -4,7 +4,12 @@
 
 <img src="https://github.com/adaptlearning/documentation/blob/master/04_wiki_assets/plug-ins/images/glossary.gif" width='548' height='497' alt="demonstration of how the glossary looks and functions" align="right"> 
 
-This extension allows for a list of glossary terms to be displayed (in alphabetical order) in the 'drawer' of the course. The terms can be clicked to reveal a definition. The list of terms is searchable and terms can be linked to from the content.
+This extension allows for a list of glossary terms to be displayed (in alphabetical order) in the 'drawer' of the course. The terms can be clicked to reveal a definition. Features and options include:  
+- the list of terms is searchable  
+- the terms' descriptions are searchable  
+- terms as they appear within course content may be linked to their associated entry in the glossary  
+- headers may be enabled to group terms by their first letter/digit  
+- an index may be enabled to make it easy to jump to group headers within a long list  
 <br><br><br><br><br><br><br><br><br><br><br><br><br>
 ## Installation
 
@@ -17,62 +22,60 @@ This extension can also be installed by adding the extension to the adapt.json f
         "adapt-contrib-glossary": "*"
 
 ## Usage
+
 You can create links to items in the glossary in the format `<a href='#' data-glossaryterm='About Framework'>Adapt Framework</a>`. You should avoid doing this in text that will be displayed within a Notify popup (e.g. question feedback) as the Glossary will open behind the Notify popup.
+
+The index links to the group headers. If the index is enabled, group headings will be employed regardless of the value of `_isGroupHeadersEnabled`.
 
 ## Settings overview
 
-For example JSON format, see [example.json](example.json). Further details are given below which can be added to course:
+The attributes listed below are used in *course.json* to configure **Glossary**, and are properly formatted as JSON in [*example.json*](https://github.com/adaptlearning/adapt-contrib-glossary/blob/master/example.json).
 
-```json
-"_glossary": {
-    "_isEnabled": true,
-    "_drawerOrder": 1,
-    "title": "Glossary",
-    "description": "Click here to view glossary for this course",
-    "searchPlaceholder": "Search",
-    "searchWithInDescriptionLabel": "Search within Description",
-    "itemNotFoundMessage": "Sorry! No results to display",
-    "_glossaryItems": [
-        {
-            "term": "",
-            "description": ""
-        }
-    ]
-}
-```
+**_glossary** (object): The Glossary object that contains values for **_isEnabled**, **_drawerOrder*, **title**, **description**, **searchItemsAlert**, **clearSearch**, **searchPlaceholder**, **searchWithInDescriptionLabel**, **itemNotFoundMessage**, **_isSearchEnabled**, **_isIndexEnabled**, **_isGroupHeadersEnabled**, and **_glossaryItems**.  
+
+>**_isEnabled** (boolean): Turns **Glossary** on and off. Acceptable values are `true` and `false`.
+
+>**_drawerOrder** (number): Determines the order in which this extension appears as a drawer item. Acceptable values are numbers.
+
+>**title** (string): This text is displayed (along with the **description**) in the [Drawer](https://github.com/adaptlearning/adapt_framework/wiki/Core-modules#drawer) as part of a button that gives access to the **Glossary**.  
+
+>**description** (string): This text is displayed (along with the **title**) in the [Drawer](https://github.com/adaptlearning/adapt_framework/wiki/Core-modules#drawer) as part of a button that gives access to the **Glossary**.  
+
+>**searchItemsAlert** (string):  This text renders as an ARIA label. It is a message indicating the number of terms in the search result. It is not visible to the naked eye, but is read by accessibility devices. A dynamic value is suggested in [*example.json*](https://github.com/adaptlearning/adapt-contrib-glossary/blob/master/example.json). 
+
+>**clearSearch** (string): This phrase is associated with the "Cancel Search" button.  
+
+>**searchPlaceholder** (string): This text appears in the Search input until the learner enters text.  
+
+>**searchWithInDescriptionLabel** (string): This text is used as a label for the "search within description" checkbox.  
+
+>**itemNotFoundMessage** (string): This text appears when a search returns no results.  
+
+>**_isSearchEnabled** (boolean): Turns the search function on and off. Acceptable values are `true` and `false`. The default is `true`. If set to `false`, the search container is not displayed.
+
+>**_isIndexEnabled** (boolean): Turns the index header on and off. Acceptable values are `true` and `false`. The default is `false`. If set to `false`, the index is not displayed. The index acts as a menu for grouped terms. It is most effective when used with long lists of terms. If **_isIndexEnabled** is set to `true`, **_isGroupHeadersEnabled** will be set to `true` when the course is running.     
+
+>**_isGroupHeadersEnabled** (boolean): Turns the group headers on and off. Terms are alphabetized and grouped by their initial character (number or letter). Acceptable values are `true` and `false`. The default is `false`.  Headers are most effective when used with long lists of terms. If **_isIndexEnabled** is set to `true`, **_isGroupHeadersEnabled** will be set to `true` when the course is running, regardless of its original value. 
+
+>**_glossaryItems** (object): This object stores properties for each glossary item. Multiple glossary items may be created. Each contains values for **term**  and **description**.  
+
+>>**term** (string): The word or phrase that comprises the glossary term.  
+
+>>**description** (string): This text is associated with each resource item. It renders as part of the aria label to tell screen readers that the content will open in an external link.
+
+<div float align=right><a href="#top">Back to Top</a></div>
 
 ## Events
 
-<table>
-    <thead>
-        <tr>
-            <td><b>Event</b></td>
-            <td><b>Description</b></td>
-            <td><b>Object</b></td>
-        </tr>
-    </thead>
-    <tr valign="top">
-        <td><i>glossary:descriptionOpen</i></td>
-        <td>Triggered when the user clicks on glossary item and its description get opened </td>
-        <td>
-            <table>
-                <tr>
-                    <td>viewId</td>
-                    <td>string</td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-</table>
+| Event   |      Description      |  Object |
+|:----------|:--------------|:-------:|
+| _glossary:descriptionOpen_ |  Triggered when the user clicks on glossary item and its description get opened | viewId (string) |
 
 ## Limitations
  
 To be completed.
 
-## Browser spec
-
-This extension has been tested to the standard Adapt browser specification.
-
+----------------------------
 **Version number:**  3.0.0   <a href="https://community.adaptlearning.org/" target="_blank"><img src="https://github.com/adaptlearning/documentation/blob/master/04_wiki_assets/plug-ins/images/adapt-logo-mrgn-lft.jpg" alt="adapt learning logo" align="right"></a>  
 **Framework versions:**  5+  
 **Author / maintainer:** Adapt Core Team with [contributors](https://github.com/adaptlearning/adapt-contrib-glossary/graphs/contributors)  
--- js/adapt-contrib-glossaryView.js
+++ js/adapt-contrib-glossaryView.js
@@ -5,7 +5,7 @@ define([
 
   var GlossaryView = Backbone.View.extend({
 
-    className: "glossary",
+    className: 'glossary',
 
     events: {
       'keyup .js-glossary-textbox-change': 'onInputTextBoxValueChange',
@@ -16,12 +16,13 @@ define([
     },
 
     itemViews: null,
+    prevScrollPos: 0,
+    isSearchActive: false,
 
     initialize: function() {
       this.listenTo(Adapt, 'remove drawer:closed', this.remove);
-
       this.setupModel();
-
+      this.onScroll = _.debounce(this.onScroll.bind(this), 200);
       this.render();
     },
 
@@ -45,6 +46,7 @@ define([
 
     setupModel: function() {
       this.arrangeGlossaryItemsToAscendingOrder();
+      this.groupGlossaryItems();
     },
 
     arrangeGlossaryItemsToAscendingOrder: function() {
@@ -56,60 +58,215 @@ define([
       this.collection.sort();
     },
 
+    groupGlossaryItems: function() {
+      // headers must be enabled when the index is in use
+      if (this.model.get('_isIndexEnabled')) {
+        this.model.set('_isGroupHeadersEnabled', true);
+      }
+      if (!this.model.get('_isGroupHeadersEnabled')) return;
+
+      /**
+       * If 'group headers' are needed, sort the entries into
+       * sets, grouped by the first character of each term
+       */
+      var groups = this.collection.groupBy(function(model) {
+        return model.get('term').charAt(0).toLocaleUpperCase();
+      });
+      this.collection._byChar0 = groups;
+    },
+
     render: function() {
-      var template = Handlebars.templates["glossary"];
+      var template = Handlebars.templates.glossary;
       this.$el.html(template(this.model.toJSON()));
-      this.renderGlossaryItems();
+
+      if (this.model.get('_isIndexEnabled')) {
+        this.renderIndexHeader();
+      }
+
+      if (this.model.get('_isGroupHeadersEnabled')) {
+        this.renderGlossaryItemsWithHeaders();
+      } else {
+        this.renderGlossaryItems();
+      }
+
       _.defer(_.bind(function() {
         this.postRender();
       }, this));
       return this;
     },
 
+    renderIndexHeader: function() {
+      var $glossaryIndex = this.$('.js-glossary-index-container').empty();
+      _.each(this.collection._byChar0, function(group, key) {
+        var template = Handlebars.templates.glossaryIndexItem;
+        $glossaryIndex.append(template({
+          _key: key,
+          _group: group
+        }));
+      });
+      this.prevScrollPos = $('.js-drawer-holder').scrollTop();
+      $('.js-drawer-holder').on('scroll', this.onScroll);
+      this.$('.js-glossary-index-link').on('click', this.scrollToPosition);
+    },
+
+    onScroll: function() {
+      var currentScrollPos = $('.js-drawer-holder').scrollTop();
+      var indexDisplay = this.$('.js-glossary-index-container').css('display');
+      var isIndexVisible = indexDisplay === 'none' ? false : true;
+
+      if (isIndexVisible) {
+        if (this.prevScrollPos > currentScrollPos) {
+          this.$('.js-glossary-index-container')
+            .addClass('scrolling-up')
+            .removeClass('scrolling-down');
+          var indexOuterHeight = this.$('.js-glossary-index-container').outerHeight(true);
+          this.$('.js-glossary-items-container').css('top', indexOuterHeight + 'px');
+        } else {
+          this.$('.js-glossary-index-container')
+            .addClass('scrolling-down')
+            .removeClass('scrolling-up');
+          this.$('.js-glossary-items-container').css('top', '0');
+        }
+      }
+
+      this.prevScrollPos = currentScrollPos;
+    },
+
+    scrollToPosition: function(event) {
+      if (event && event.preventDefault) event.preventDefault();
+      var selector = $(event.currentTarget).attr('href');
+      var $target = $(selector);
+      $('.js-drawer-holder').animate({scrollTop: ($target.position().top)})
+    },
+
+    renderGlossaryItemsWithHeaders: function() {
+      this.itemViews = [];
+      var $glossaryItemContainer = this.$('.js-glossary-items-container').empty();
+
+      _.each(this.collection._byChar0, function(group, key) {
+        var $glossaryItemsGroupContainer = $("<div" +
+          " class='glossary__items-group' role=list'" +
+          " aria-labelledby='" + key + "'></div>");
+        var $glossaryItemsGroupHeader = $("<div id=" + key
+          + " class='glossary__items-group-header js-glossary-items-group-header'>" + key + "</div>");
+        $glossaryItemsGroupContainer.append($glossaryItemsGroupHeader);
+        this.createItemViews(group, $glossaryItemsGroupContainer);
+        $glossaryItemContainer.append($glossaryItemsGroupContainer);
+      }, this);
+    },
+
     renderGlossaryItems: function() {
       this.itemViews = [];
-      var $glossaryItemContainer = this.$('.glossary__items-container').empty();
-      _.each(this.collection.models, function(item, index) {
+      var $glossaryItemContainer = this.$('.js-glossary-items-container').empty();
+      this.createItemViews(this.collection.models, $glossaryItemContainer);
+    },
+
+    createItemViews: function(models, $container) {
+      _.each(models, function(item) {
         var itemView = new GlossaryItemView({model: item});
-        itemView.$el.appendTo($glossaryItemContainer);
+        itemView.$el.appendTo($container);
         // store a reference to each of the views so that checkForTermToShow can search through them
         this.itemViews.push(itemView);
       }, this);
     },
 
     postRender: function() {
-      this.checkForTermToShow();
+      var widthExclScrollbar = $('.drawer').prop('clientWidth');
+      $('.drawer__toolbar').css({
+        'width': widthExclScrollbar + 'px',
+        'z-index': '2'
+      });
 
       this.listenTo(Adapt, {
         'drawer:openedItemView': this.remove,
         'drawer:triggerCustomView': this.remove
       });
+
+      this.configureContainers();
+      this.checkForTermToShow();
+    },
+
+    configureContainers: function() {
+      var isSearchEnabled = this.model.get('_isSearchEnabled');
+      var isIndexEnabled = this.model.get('_isIndexEnabled');
+      var glossaryWidth = this.$('.js-glossary-inner').width();
+      var searchOuterHeight = this.$('.js-glossary-search-container').outerHeight(true);
+
+      if (isSearchEnabled) {
+        this.$('.js-glossary-search-container')
+          .css('width', glossaryWidth);
+        this.$('.js-glossary-item-not-found').css('top', searchOuterHeight + 'px');
+      }
+
+      if (isIndexEnabled) {
+        this.$('.js-glossary-index-container')
+          .css('margin-top', searchOuterHeight)
+          .css('width', glossaryWidth);
+      }
+
+      if (isSearchEnabled && !isIndexEnabled) {
+        this.$('.js-glossary-items-container').css('top', searchOuterHeight + 'px');
+      }
+
+      if (isSearchEnabled && isIndexEnabled) {
+        this.$('.js-glossary-items-container').css('top', 'unset');
+      }
+
+      if (this.isSearchActive && isIndexEnabled) {
+        this.$('.js-glossary-items-container').css('top', searchOuterHeight);
+      }
     },
 
     onInputTextBoxValueChange: _.debounce(function(event) {
       this.showItemNotFoundMessage(false);
+      this.isSearchActive = true;
       var searchItem = this.$('.js-glossary-textbox-change').val().toLowerCase();
-      var shouldSearchInDescription = this.$('.js-glossary-checkbox-change').is(":checked");
+      var shouldSearchInDescription = this.$('.js-glossary-checkbox-change').is(':checked');
 
-      var searchItemsAlert = this.model.get("searchItemsAlert") || "";
+      var searchItemsAlert = this.model.get('searchItemsAlert') || "";
 
       if (searchItem.length > 0) {
         this.$('.js-glossary-cancel-btn-click').removeClass('u-display-none');
         this.$('.js-glossary-search-icon').addClass('u-display-none');
         var filteredItems = this.getFilteredGlossaryItems(searchItem, shouldSearchInDescription);
-        this.$('.glossary__alert').html(Handlebars.compile(searchItemsAlert)({ filteredItems: filteredItems }));
+        this.$('.js-glossary-alert').html(Handlebars.compile(searchItemsAlert)({filteredItems: filteredItems}));
+        this.hideHeaders();
         this.showFilterGlossaryItems(filteredItems);
+        this.configureContainers();
       } else {
         this.$('.js-glossary-cancel-btn-click').addClass('u-display-none');
         this.$('.js-glossary-search-icon').removeClass('u-display-none');
+        this.isSearchActive = false;
+        this.showHeaders();
         this.showGlossaryItems(true);
+        this.configureContainers();
       }
+      $('.js-drawer-holder').animate({scrollTop: '0'});
     }, 200),
 
+    hideHeaders: function() {
+      if (this.model.get('_isIndexEnabled')) {
+        this.$('.js-glossary-index-container').addClass('u-display-none');
+      }
+      if (this.model.get('_isGroupHeadersEnabled')) {
+        this.$('.js-glossary-items-group-header').addClass('u-display-none');
+      }
+    },
+
+    showHeaders: function() {
+      if (this.model.get('_isIndexEnabled')) {
+        this.$('.js-glossary-index-container').removeClass('u-display-none');
+      }
+      if (this.model.get('_isGroupHeadersEnabled')) {
+        this.$('.js-glossary-items-group-header').removeClass('u-display-none');
+      }
+    },
+
     onCancelButtonClick: function(event) {
-      if(event && event.preventDefault) event.preventDefault();
+      if (event && event.preventDefault) event.preventDefault();
+      this.isSearchActive = false;
       var $input = this.$('.js-glossary-textbox-change');
-      $input.val("").trigger("input");
+      $input.val("").trigger('input');
       _.defer(function() {
         $input.focus();
       });
@@ -125,8 +282,8 @@ define([
           var description = model.get('description').toLowerCase();
 
           return shouldSearchInDescription ?
-              title.indexOf(term) !== -1 || description.indexOf(term) !== -1 :
-              title.indexOf(term) !== -1;
+            title.indexOf(term) !== -1 || description.indexOf(term) !== -1 :
+            title.indexOf(term) !== -1;
         });
       });
     },
@@ -134,7 +291,7 @@ define([
     // show only the filtered glossary items or no item found message
     showFilterGlossaryItems: function(filteredItems) {
       this.showGlossaryItems(false);
-      if(filteredItems.length > 0) {
+      if (filteredItems.length > 0) {
         _.each(filteredItems, function(item, index) {
           item.set('_isVisible', true);
         });
@@ -145,7 +302,7 @@ define([
 
     // show/hide the item not found message.
     showItemNotFoundMessage: function(_isVisible) {
-      var $itemNotFound = this.$('.glossary__item-not-found');
+      var $itemNotFound = this.$('.js-glossary-item-not-found');
 
       if (!_isVisible && !$itemNotFound.hasClass('u-display-none')) {
         $itemNotFound.addClass('u-display-none');

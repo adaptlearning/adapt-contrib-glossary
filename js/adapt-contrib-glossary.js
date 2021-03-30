463f7df226f12df65b6579968495a399523151b8
Eleanor Heath <eleanor.heath@learningpoool.com>
2021-03-30 13:26:29 +0100

    delete node modules

--- js/adapt-contrib-glossary.js
+++ js/adapt-contrib-glossary.js
@@ -1,58 +1,54 @@
-define([
-  'core/js/adapt',
-  './adapt-contrib-glossaryView'
-], function(Adapt, GlossaryView) {
+import Adapt from 'core/js/adapt';
+import GlossaryView from './adapt-contrib-glossaryView';
 
-  function setupGlossary(glossaryModel, glossaryItems) {
+function setupGlossary(glossaryModel, glossaryItems) {
 
-    glossaryModel = new Backbone.Model(glossaryModel);
+  glossaryModel = new Backbone.Model(glossaryModel);
 
-    const options = {
-      model: glossaryModel,
-      collection: new Backbone.Collection(glossaryItems)
+  const options = {
+    model: glossaryModel,
+    collection: new Backbone.Collection(glossaryItems)
+  };
+
+  Adapt.on('glossary:showGlossary', () => {
+    Adapt.drawer.triggerCustomView(new GlossaryView(options).$el);
+  });
+
+  /**
+   * handler for links in the content in the following format:
+   * <a href='#' data-glossaryterm='term name'>glossary term link</a>
+   * these links should trigger the glossary to open with that term automatically selected
+   */
+  $('body').on('click.glossary', 'a[data-glossaryterm]', e => {
+    if (e) e.preventDefault();
+
+    const newoptions = Object.assign({}, options);
+    newoptions.attributes = {
+      'data-termtoshow': e.currentTarget.getAttribute('data-glossaryterm')
     };
 
-    Adapt.on('glossary:showGlossary', () => {
-      Adapt.drawer.triggerCustomView(new GlossaryView(options).$el);
-    });
+    Adapt.drawer.triggerCustomView(new GlossaryView(newoptions).$el);
+  });
+}
 
-    /**
-     * handler for links in the content in the following format:
-     * <a href='#' data-glossaryterm='term name'>glossary term link</a>
-     * these links should trigger the glossary to open with that term automatically selected
-     */
-    $('body').on('click.glossary', 'a[data-glossaryterm]', e => {
-      if (e) e.preventDefault();
+function initGlossary() {
+  const courseGlossary = Adapt.course.get('_glossary');
 
-      const newoptions = Object.assign({}, options);
-      newoptions.attributes = {
-        'data-termtoshow': e.currentTarget.getAttribute('data-glossaryterm')
-      };
-
-      Adapt.drawer.triggerCustomView(new GlossaryView(newoptions).$el);
-    });
+  // do not proceed until glossary enabled on course.json
+  if (!courseGlossary || !courseGlossary._isEnabled) {
+    return;
   }
 
-  function initGlossary() {
-    const courseGlossary = Adapt.course.get('_glossary');
+  const drawerObject = {
+    title: courseGlossary.title,
+    description: courseGlossary.description,
+    className: 'is-glossary',
+    drawerOrder: courseGlossary._drawerOrder || 0
+  };
 
-    // do not proceed until glossary enabled on course.json
-    if (!courseGlossary || !courseGlossary._isEnabled) {
-      return;
-    }
+  Adapt.drawer.addItem(drawerObject, 'glossary:showGlossary');
 
-    const drawerObject = {
-      title: courseGlossary.title,
-      description: courseGlossary.description,
-      className: 'is-glossary',
-      drawerOrder: courseGlossary._drawerOrder || 0
-    };
+  setupGlossary(courseGlossary, courseGlossary._glossaryItems);
+}
 
-    Adapt.drawer.addItem(drawerObject, 'glossary:showGlossary');
-
-    setupGlossary(courseGlossary, courseGlossary._glossaryItems);
-  }
-
-  Adapt.on('app:dataReady', initGlossary);
-
-});
+Adapt.on('app:dataReady', initGlossary);
--- js/adapt-contrib-glossaryItemView.js
+++ js/adapt-contrib-glossaryItemView.js
@@ -1,113 +1,107 @@
-define([
-  'core/js/adapt'
-], function(Adapt) {
+import Adapt from 'core/js/adapt';
 
-  class GlossaryItemView extends Backbone.View {
+export default class GlossaryItemView extends Backbone.View {
 
-    className() {
-      return 'glossary__item';
+  className() {
+    return 'glossary__item';
+  }
+
+  attributes() {
+    return {
+      role: 'listitem'
+    };
+  }
+
+  events() {
+    return {
+      'click .js-glossary-item-term-click': 'onGlossaryItemClicked'
+    };
+  }
+
+  initialize() {
+    this.listenTo(Adapt, {
+      'remove drawer:closed': this.remove,
+      'glossary:descriptionOpen': this.descriptionOpen
+    });
+    this.setupModel();
+    this.listenTo(this.model, 'change:_isVisible', this.onGlossaryItemVisibilityChange);
+    this.render();
+  }
+
+  setupModel() {
+    this.model.set({
+      '_isVisible': true,
+      '_isDescriptionOpen': false
+    });
+  }
+
+  render() {
+    const template = Handlebars.templates.glossaryItem;
+    this.$el.html(template(this.model.toJSON()));
+    _.defer(this.postRender.bind(this));
+    return this;
+  }
+
+  postRender() {
+    this.listenTo(Adapt, {
+      'drawer:openedItemView': this.remove,
+      'drawer:triggerCustomView': this.remove
+    });
+  }
+
+  onGlossaryItemClicked(event) {
+    event && event.preventDefault();
+    Adapt.trigger('glossary:descriptionOpen', this.model.cid);
+  }
+
+  toggleGlossaryItemDescription() {
+    if (this.model.get('_isDescriptionOpen')) {
+      this.hideGlossaryItemDescription();
+      return;
     }
 
-    attributes() {
-      return {
-        role: 'listitem'
-      };
+    this.showGlossaryItemDescription();
+  }
+
+  /**
+   * show the glossary item description and highlight the selected term.
+   */
+  showGlossaryItemDescription() {
+    const $glossaryItemTerm = this.$('.js-glossary-item-term-click');
+    const $description = $glossaryItemTerm.addClass('is-selected').siblings('.js-glossary-item-description').slideDown(200, () => {
+      Adapt.a11y.focusFirst($description, { defer: true });
+    });
+    $glossaryItemTerm.attr('aria-expanded', true);
+    this.model.set('_isDescriptionOpen', true);
+  }
+
+  /**
+   * hide the glossary item description and un-highlight the selected term.
+   */
+  hideGlossaryItemDescription() {
+    this.$('.js-glossary-item-description').stop(true, true).slideUp(200);
+    this.model.set('_isDescriptionOpen', false);
+
+    this.$('.js-glossary-item-term-click').removeClass('is-selected').attr('aria-expanded', false);
+  }
+
+  // This function will decide whether this glossary item's description should be visible or not.
+  descriptionOpen(viewId) {
+    if (viewId == this.model.cid) {
+      this.toggleGlossaryItemDescription();
+      return;
     }
 
-    events() {
-      return {
-        'click .js-glossary-item-term-click': 'onGlossaryItemClicked'
-      };
-    }
+    if (!this.model.get('_isDescriptionOpen')) return;
+    this.hideGlossaryItemDescription();
+  }
 
-    initialize() {
-      this.listenTo(Adapt, {
-        'remove drawer:closed': this.remove,
-        'glossary:descriptionOpen': this.descriptionOpen
-      });
-      this.setupModel();
-      this.listenTo(this.model, 'change:_isVisible', this.onGlossaryItemVisibilityChange);
-      this.render();
-    }
-
-    setupModel() {
-      this.model.set({
-        '_isVisible': true,
-        '_isDescriptionOpen': false
-      });
-    }
-
-    render() {
-      const template = Handlebars.templates.glossaryItem;
-      this.$el.html(template(this.model.toJSON()));
-      _.defer(this.postRender.bind(this));
-      return this;
-    }
-
-    postRender() {
-      this.listenTo(Adapt, {
-        'drawer:openedItemView': this.remove,
-        'drawer:triggerCustomView': this.remove
-      });
-    }
-
-    onGlossaryItemClicked(event) {
-      event && event.preventDefault();
-      Adapt.trigger('glossary:descriptionOpen', this.model.cid);
-    }
-
-    toggleGlossaryItemDescription() {
-      if (this.model.get('_isDescriptionOpen')) {
-        this.hideGlossaryItemDescription();
-        return;
-      }
-
-      this.showGlossaryItemDescription();
-    }
-
-    /**
-     * show the glossary item description and highlight the selected term.
-     */
-    showGlossaryItemDescription() {
-      const $glossaryItemTerm = this.$('.js-glossary-item-term-click');
-      const $description = $glossaryItemTerm.addClass('is-selected').siblings('.js-glossary-item-description').slideDown(200, () => {
-        Adapt.a11y.focusFirst($description, { defer: true });
-      });
-      $glossaryItemTerm.attr('aria-expanded', true);
-      this.model.set('_isDescriptionOpen', true);
-    }
-
-    /**
-     * hide the glossary item description and un-highlight the selected term.
-     */
-    hideGlossaryItemDescription() {
-      this.$('.js-glossary-item-description').stop(true, true).slideUp(200);
-      this.model.set('_isDescriptionOpen', false);
-
-      this.$('.js-glossary-item-term-click').removeClass('is-selected').attr('aria-expanded', false);
-    }
-
-    // This function will decide whether this glossary item's description should be visible or not.
-    descriptionOpen(viewId) {
-      if (viewId == this.model.cid) {
-        this.toggleGlossaryItemDescription();
-        return;
-      }
-
-      if (!this.model.get('_isDescriptionOpen')) return;
+  // This function should call upon glossary item model attribute '_isVisible' gets change.
+  onGlossaryItemVisibilityChange() {
+    if (this.model.get('_isDescriptionOpen')) {
       this.hideGlossaryItemDescription();
     }
 
-    // This function should call upon glossary item model attribute '_isVisible' gets change.
-    onGlossaryItemVisibilityChange() {
-      if (this.model.get('_isDescriptionOpen')) {
-        this.hideGlossaryItemDescription();
-      }
-
-      this.$el.toggleClass('u-display-none', !this.model.get('_isVisible'));
-    }
-  };
-
-  return GlossaryItemView;
-
-});
+    this.$el.toggleClass('u-display-none', !this.model.get('_isVisible'));
+  }
+}
